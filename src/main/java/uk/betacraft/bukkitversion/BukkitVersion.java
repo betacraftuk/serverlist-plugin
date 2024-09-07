38e7ba110d4ea9f7b7b09447d5b97d9221085199
Moresteck <moresteck@protonmail.com>
2024-09-07 15:28:40 +0000

    Added gradle

--- src/main/java/uk/betacraft/bukkitversion/BukkitVersion.java
+++ src/main/java/uk/betacraft/bukkitversion/BukkitVersion.java
@@ -24,6 +24,7 @@ import org.bukkit.World.Environment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import uk.betacraft.serverlist.BCPing;
@@ -331,7 +332,16 @@ public class BukkitVersion {
      */
     public void registerEvent(String type, Listener listener, String priority) {
         if (isBukkitNewSystem()) {
-            this.plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+            PluginManager pluginManager = this.plugin.getServer().getPluginManager();
+
+            try {
+                Method registerEventsMethod = pluginManager.getClass()
+                        .getDeclaredMethod("registerEvents", listener.getClass(), plugin.getClass());
+                
+                registerEventsMethod.invoke(pluginManager, listener, plugin);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
         } else {
             this.plugin.getServer().getPluginManager().registerEvent(org.bukkit.event.Event.Type.valueOf(type), 
                     listener, org.bukkit.event.Event.Priority.valueOf(priority), plugin);
